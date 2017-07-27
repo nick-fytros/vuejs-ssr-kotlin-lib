7cc533d03ebd2f2055800ec1bc458c9d21b4381b
Nikos Fytros <fytros.n@gmail.com>
2017-07-27 09:32:29 +0200

    removed idea workspace xml

--- src/main/kotlin/renderer.kt
+++ src/main/kotlin/renderer.kt
@@ -22,18 +22,5 @@ class Renderer(laygoutFilePath: String?, componentsFolderPath: String?) {
 }
 
 fun main(args: Array<String>) {
-//    val nodeJS = NodeJS.createNodeJS()
-//    val callback = JavaCallback { receiver, parameters -> "Hello, Kotlin + NodeJs with uuid: " + parameters.get(0) }
-//
-//    nodeJS.runtime.registerJavaMethod(callback, "someKotlinMethod")
-//
-//    val nodeScript = JavascriptFile("app.js").jsFile
-//
-//    nodeJS.exec(nodeScript)
-//
-//    while (nodeJS.isRunning) {
-//        nodeJS.handleMessage()
-//    }
-//    nodeJS.release()
     Renderer(null, null).render(null)
 }
