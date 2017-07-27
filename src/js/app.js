fed75e4b0341f341cb3844b963e54a18513c5613
Nikos Fytros <fytros.n@gmail.com>
2017-07-27 16:35:41 +0200

    getting closer on getting expressVue renderer to work with j2v8

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 // Step 1: Create a Vue instance
 const Vue = require('vue')
-// const ExpressVueRenderer = require('express-vue-renderer')
+const ExpressVueRenderer = require('express-vue-renderer')
 const path = require('path');
 // test mixin functionality too
 const exampleMixin = {
@@ -13,8 +13,8 @@ const exampleMixin = {
 // options object
 const options = {
     rootPath: path.join(__dirname),
-    viewsPath: '',
-    componentsPath: 'vueComponents',
+    viewsPath: path.join(__dirname, 'vue'),
+    componentsPath: 'vue/components',
     layout: {
         start: '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">',
         middle: '<body><div id="app">',
@@ -33,7 +33,7 @@ const options = {
     }
 };
 // create the renderer with the options
-// const Renderer = new ExpressVueRenderer(options);
+const Renderer = new ExpressVueRenderer(options);
 // data and options object to be passed from the backend
 const data = {
     title: 'Express Vue',
@@ -46,15 +46,15 @@ const vueOptions = {
     }
 }
 
-// Renderer.renderToStream('main.vue', data, vueOptions)
-//     .then(stream => {
-//         stream.on('data', chunk => res.write(chunk));
-//         stream.on('end', () => res.end());
-//     })
-//     .catch(error => {
-//         console.error(error);
-//         res.send(error);
-//     });
+var HtmlChunks = ''
+Renderer.renderToStream('main.vue', data, vueOptions)
+    .then(stream => {
+        stream.on('data', chunk => HtmlChunks += chunk);
+        stream.on('end', () => renderCallback(HtmlChunks));
+    })
+    .catch(error => {
+        console.error(error);
+    });
 
 // Step 2: Create a renderer
 const renderer = require('vue-server-renderer').createRenderer()
---
+++ src/js/vue/main.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <header></header>
+        <h1>{{title}}</h1>
+        <p>Welcome to the {{title}} demo. Click a link:</p>
+        <input v-model="message" placeholder="edit me">
+        <p>{{message}}</p>
+        <button type="button" name="button" v-on:click="this.hello">Test mixin</button>
+        <button type="button" name="button" v-on:click="this.test">Test method</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+        }
+    },
+    methods: {
+        test: function () {
+            console.error('test');
+        }
+    }
+}
+</script>
+
+<style lang="css">
+
+</style>
